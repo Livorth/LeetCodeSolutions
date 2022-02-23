b4d7458701daf4f15c864a564488ce2eced97ad6
Livorth <livorth@163.com>
2022-02-23 11:22:38 +0800

    auto save

--- autoSave.bat
+++ autoSave.bat
@@ -1,5 +1,4 @@
-I: 
-cd 3.CodeFile\0.markdown\LeetCodeSolutions
+cd I:\3.CodeFile\0.markdown\LeetCodeSolutions
 git add .                           
 git commit -m "auto save"
 git push -u origin master 
