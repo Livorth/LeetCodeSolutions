012d1ceb39f7796a07039e7966e97ea2fb223cbb
livorth <1770266167@qq.com>
2023-11-25 13:49:06 +0800

    1457. 二叉树中的伪回文路径

---
+++ code/src/main/java/daily/leetcode/editor/cn/Question1457.java
@@ -0,0 +1,81 @@
+package daily.leetcode.editor.cn;
+
+import java.util.*;
+
+public class Question1457 {
+    public static void main(String[] args) {
+        Solution solution = new Question1457().new Solution();
+
+
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+        public int pseudoPalindromicPaths(TreeNode root) {
+            if (root == null) return 0;
+            return dfs(root, 0);
+        }
+
+        private int dfs(TreeNode root, int k) {
+            int res = 0;
+            // 左移val次，然后异或
+            // 偶数次出现则会置为0，奇数次出现则为1
+            int n = k ^ (1 << root.val);
+            // 到达叶子节点时
+            if (root.left == null && root.right == null) {
+                // 回文判断
+                // n==0判断回文串为偶数长度的情况下,所有二进制位是否都为0
+                // n&(n-1)判断奇数情况下是否只有一个二进制位为1
+                if (n == 0 || (n & (n - 1)) == 0) {
+                    return 1;
+                }
+            }
+            // 继续遍历左右树
+            if (root.left != null) {
+                res += dfs(root.left, n);
+            }
+            if (root.right != null) {
+                res += dfs(root.right, n);
+            }
+            return res;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
