5dbb66a3f4dd751e328e61887f0fb383ddbf360c
livorth <1770266167@qq.com>
2023-12-04 14:08:05 +0800

    538. 把二叉搜索树转换为累加树

---
+++ code/src/main/java/daily/leetcode/editor/cn/Question538.java
@@ -0,0 +1,57 @@
+package daily.leetcode.editor.cn;
+
+import java.util.*;
+
+public class Question538 {
+    public static void main(String[] args) {
+        Solution solution = new Question538().new Solution();
+
+
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+
+    /**
+     * 执行耗时:0 ms,击败了100.00% 的Java用户
+     * 内存消耗:43.4 MB,击败了5.35% 的Java用户
+     */
+    class Solution {
+
+        // sum维护当期节点右数节点大小和
+        private int sum = 0;
+
+        public TreeNode convertBST(TreeNode root) {
+            dfs(root);
+            return root;
+        }
+
+        private void dfs(TreeNode root) {
+            if (root == null) return;
+            dfs(root.right);
+            sum += root.val;
+            root.val = sum;
+            dfs(root.left);
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
