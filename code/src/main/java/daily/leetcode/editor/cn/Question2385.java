50cd8e1f28b889101ee1b34cc8534bc0efda70b3
livorth <1770266167@qq.com>
2024-07-08 11:20:34 +0800

    2385. 感染二叉树需要的总时间

--- code/src/main/java/daily/leetcode/editor/cn/Question2385.java
+++ code/src/main/java/daily/leetcode/editor/cn/Question2385.java
@@ -28,43 +28,98 @@ public class Question2385 {
 
     //leetcode submit region begin(Prohibit modification and deletion)
 
-
-    class Solution {
-        int max = 0;
-
-        private int dfs(TreeNode root, int start, int time) {
-            if (root == null) return -1;
-            // 如果本节点是start，那么从当前节点开始感染
-            if (time == -1 && root.val == start) time = 0;
-
-            if (time != -1) {
-                dfs(root.left, start, time + 1);
-                dfs(root.right, start, time + 1);
-                if (time > max) max = time;
-                return time + 1;
-            } else {
-                int tem = dfs(root.left, start, time);
-                if (tem != -1) {//左子树被感染，传递到右子树
-                    time = tem;
-                    dfs(root.right, start, time + 1);
-                } else {//右子树被感染，传递到左子树
-                    tem = dfs(root.right, start, time);
-                    if (tem != -1) {
-                        time = tem;
-                        dfs(root.left, start, time + 1);
-                    }
-                }
-            }
-            if (time > max) max = time;
-            if (time != -1) return time + 1;
-            return -1;
-        }
+    /**
+     * 单次dfs，直接在dfs计算出两种情况
+     * 执行耗时:12 ms,击败了79.17% 的Java用户
+     * 内存消耗:77.8 MB,击败了69.17% 的Java用户
+     */
+    class Solution1 {
+        int res = 0;
+        int depth = -1;
 
         public int amountOfTime(TreeNode root, int start) {
-            dfs(root, start, -1);
-            return max;
+            dfs(root, 0, start);
+            return res;
+        }
+
+        /**
+         * @param node
+         * @param level 当前深度
+         * @param start
+         * @return 后边还有多深
+         */
+        private int dfs(TreeNode node, int level, int start) {
+            if (node == null) return 0;
+            // 记录start的深度
+            if (node.val == start) this.depth = level;
+
+            int ldpt = dfs(node.left, level + 1, start);
+            // start是否在左子树，如果不在那肯定就在右边
+            boolean inL = this.depth != -1 ? true : false;
+            int rdpt = dfs(node.right, level + 1, start);
+            // 1. 计算以当前节点为根节点的树所需要的时间
+            if (inL) {
+                // 从start出发到node的距离 + node另一侧子树的深度
+                this.res = Math.max(res, depth - level + rdpt);
+            } else {
+                this.res = Math.max(res, depth - level + ldpt);
+            }
+            // 2. 当前点就是start，而且最深路径就在start子树中
+            if (node.val == start) {
+                res = Math.max(res, Math.max(ldpt, rdpt));
+            }
+            return Math.max(ldpt, rdpt) + 1;
+        }
+
+
+        int dfs1(TreeNode root, int level, int start) {
+            if (root == null) return 0;
+            if (root.val == start) depth = level;                       // 当前节点即起始节点
+            int l = dfs(root.left, level + 1, start);                   // 左子树的树高
+            boolean inLeft = depth != -1;                               // 起始节点是否在左子树上
+            int r = dfs(root.right, level + 1, start);                  // 右子树的树高
+            if (root.val == start) res = Math.max(res, Math.max(l, r)); // 情况1：感染以 start 为根结点的树所需时间
+            if (inLeft) res = Math.max(res, depth - level + r);         // 情况2：感染以 root 为根结点的树所需时间
+            else res = Math.max(res, depth - level + l);
+            return Math.max(l, r) + 1;                                  // 返回树高
         }
     }
+
+    /**
+     * 单次dfs
+     * 执行耗时:12 ms,击败了87.75% 的Java用户
+     * 内存消耗:77.3 MB,击败了80.15% 的Java用户
+     */
+    class Solution {
+        int res = 0;
+        int sd = -1;
+
+        public int amountOfTime(TreeNode root, int start) {
+            dfs(root, 0, start);
+            return res;
+        }
+
+        private int dfs(TreeNode root, int depth, int start) {
+            if (root == null) return 0;
+            if (root.val == start) sd = depth;
+            int left = dfs(root.left, depth + 1, start);
+            boolean inLeft = sd != -1;
+            int right = dfs(root.right, depth + 1, start);
+            // start往下出发的情况，即start的两颗子树的深度
+            if (root.val == start) {
+                res = Math.max(res, Math.max(left, right));
+            }
+            // start往上出发的情况，即start的每个父节点的另一棵子树的情况
+            if (inLeft) {
+                res = Math.max(res, right + sd - depth);
+            } else {
+                res = Math.max(res, left + sd - depth);
+            }
+            // 返回root节点的树高
+            return Math.max(left, right) + 1;
+        }
+    }
+
 
 //leetcode submit region end(Prohibit modification and deletion)
 
